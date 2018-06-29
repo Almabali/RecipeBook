8c102cab0b94ee03cecc180c660a838f0180ed5b
Almabali <balazs.almady@gmail.com>
2018-06-29 21:23:43 +0200

    Recipe model, recipe-list skeleton S3/L48

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: "app-recipe-list",
@@ -6,6 +7,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "testRecipe",
+      "Yummy",
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/" +
+        "4_3_horizontal_-_900x675/public/wild-mushroom-farfalle-ck.jpg?itok=urP4jrLS"
+    )
+  ];
+
   constructor() {}
 
   ngOnInit() {}
