89357d1a425dfb97a2781d99fa30de4363115c6c
Almabali <balazs.almady@gmail.com>
2018-07-04 21:50:02 +0200

    recipe selection works. Recipe-detail refined. S6/80

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from "../recipe.model";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  @Output() selectRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe(
       "testRecipe",
@@ -23,6 +24,10 @@ export class RecipeListComponent implements OnInit {
   ];
 
   constructor() {}
+
+  onSelectRecipe(recipe: Recipe): void {
+    this.selectRecipe.emit(recipe);
+  }
 
   ngOnInit() {}
 }
