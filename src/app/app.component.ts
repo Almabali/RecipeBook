ce8a4dc563717914ad8caa10a47ef3fcdab54c49
Almabali <balazs.almady@gmail.com>
2018-07-04 20:51:38 +0200

    Basic navigation added. S6/78

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  selectedPage = "Recipes";
+
+  onPageChange(selectedPage: string): void {
+    this.selectedPage = selectedPage;
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-header",
   templateUrl: "header.component.html"
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() selectedPage: EventEmitter<string> = new EventEmitter<string>();
+
+  onChangeSelectedPage(selectedPage: string): void {
+    this.selectedPage.emit(selectedPage);
+  }
+}
