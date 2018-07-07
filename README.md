7be3a0db053dbdbf6a34323c3315e6f5bc132e96
Almabali <balazs.almady@gmail.com>
2018-07-07 10:20:30 +0200

    Ingredients added to recipe. S10/111

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # RecipeBook
-Project for Udemy Angular course
+
+Project for Udemy Angular course: www.udemy.com/the-complete-guide-to-angular-2
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -9,13 +10,19 @@ export class RecipeService {
       "testRecipe",
       "Yummy",
       "http://cdn-image.myrecipes.com/sites/default/files/styles/" +
-        "4_3_horizontal_-_900x675/public/wild-mushroom-farfalle-ck.jpg?itok=urP4jrLS"
+        "4_3_horizontal_-_900x675/public/wild-mushroom-farfalle-ck.jpg?itok=urP4jrLS",
+      [
+        new Ingredient("Meat", 1),
+        new Ingredient("Bread", 2),
+        new Ingredient("Tomato", 5)
+      ]
     ),
     new Recipe(
       "Recipe 2",
       "Tasty",
       "http://cdn-image.myrecipes.com/sites/default/files/styles/" +
-        "4_3_horizontal_-_900x675/public/wild-mushroom-farfalle-ck.jpg?itok=urP4jrLS"
+        "4_3_horizontal_-_900x675/public/wild-mushroom-farfalle-ck.jpg?itok=urP4jrLS",
+      [new Ingredient("Strawberry", 20), new Ingredient("Flakes", 1000)]
     )
   ];
 
