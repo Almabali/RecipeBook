4a89fd1136609837927c94e119291594a8b7b5ad
Almabali <balazs.almady@gmail.com>
2018-07-21 16:29:54 +0200

    Added Recipe Edit frame. S12/154

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from "@angular/core";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 
 const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
@@ -13,7 +14,9 @@ const appRoutes: Routes = [
     component: RecipesComponent,
     children: [
       { path: "", component: RecipeStartComponent },
-      { path: ":id", component: RecipeDetailComponent }
+      { path: "new", component: RecipeEditComponent },
+      { path: ":id", component: RecipeDetailComponent },
+      { path: ":id/edit", component: RecipeEditComponent }
     ]
   },
   { path: "**", redirectTo: "/recipes" }
